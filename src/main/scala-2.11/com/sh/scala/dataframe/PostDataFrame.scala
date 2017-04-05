e133c2742dbeb66fad6e4be196478cbbc6057fee
alekssh <alekssh@gmail.com>
2017-04-06 00:04:35 +0300

    spark

--- src/main/scala-2.11/com/sh/scala/dataframe/PostDataFrame.scala
+++ src/main/scala-2.11/com/sh/scala/dataframe/PostDataFrame.scala
@@ -14,16 +14,17 @@ object PostDataFrame {
       .master("local[*]")
       .getOrCreate()
 
+    import spark.implicits._
+
     val sc = spark.sparkContext
 
     val posts = sc.textFile("src/main/resources/italianPosts.csv").map(_.split("~"))
-
     posts.cache()
 
-/*
-    val postsDfTuple = DataFrameCreator.viaTuple(spark, posts)
-    val postsDfCase = DataFrameCreator.viaCase(spark, posts)
-*/
+    /*
+        val postsDfTuple = DataFrameCreator.viaTuple(spark, posts)
+        val postsDfCase = DataFrameCreator.viaCase(spark, posts)
+    */
     val postsDf = DataFrameCreator.viaStruct(spark, posts)
 
     postsDf.printSchema()
@@ -32,8 +33,19 @@ object PostDataFrame {
     val idAndBody = postsDf.select("id", "body")
     idAndBody.show(3)
 
-  }
+    postsDf.select($"body" contains "Italiano").show(3)
 
+    val questions = postsDf.where($"postTypeId" === 1)
+
+    questions.withColumn("ratio", 'viewCount / 'score).where('ratio < 35).show()
+
+    questions.sort('lastActivityDate desc).show(10)
+
+    import org.apache.spark.sql.functions._
+
+    println(questions.withColumn("activePeriod", datediff('lastActivityDate, 'creationDate))
+      .orderBy('activePeriod desc).head.getString(3).replace("&lt;", "<").replace("&gt;", ">"))
+  }
 
 }
 
